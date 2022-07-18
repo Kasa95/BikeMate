1ad642f1aada53ecb1f5f825c68c37e9f5f6ddaf
Kasa95 <nachocasanova95@gmail.com>
2022-07-18 21:43:22 +0000

    diseño de bsuqueda mejorado

--- src/front/js/pages/search.js
+++ src/front/js/pages/search.js
@@ -54,7 +54,7 @@ export const Search = () => {
   return (
     <>
       <div className="container-fluid text-center orange-searchman-background">
-        <img src={searchMan} alt="" style={{ width: "500px" }} />
+        <img src={searchMan} alt="" className="search-header-img" />
         <div className="centered">
           <h1>Find new groups and mates</h1>
           <p>
@@ -65,128 +65,118 @@ export const Search = () => {
       </div>
       {/* busqueda empieza aquí */}
       <div className="container">
-        <div className="row d-flex justify-content-center align-items-center border-1 mt-4">
-          <div className="col-md-10">
-            <div className="row">
-              <div className="col-3 d-flex justify-content-around align-items-center">
-                <div className="btn-group w-100">
-                  <input
-                    type="radio"
-                    className="btn-check"
-                    name="options"
-                    id="searchUser"
-                    autoComplete="off"
-                    // checked="true"
-                    defaultChecked={searchType === "user" ? true : false}
-                    onChange={() => setSearchType("user")}
-                  />
-                  <label className="btn btn-primary" htmlFor="searchUser">
-                    Users
-                  </label>
-                  <input
-                    type="radio"
-                    className="btn-check"
-                    name="options"
-                    id="searchGroup"
-                    autoComplete="off"
-                    defaultChecked={searchType === "group" ? true : false}
-                    onChange={() => setSearchType("group")}
-                  />
-                  <label className="btn btn-primary" htmlFor="searchGroup">
-                    Groups
-                  </label>
-                </div>
-              </div>
-              <div className="search col-9">
-                <i className="fa fa-search"> </i>
-                <input
-                  type="search"
-                  name="search-form"
-                  id="search-form"
-                  className="form-control"
-                  placeholder={
-                    searchType === "user"
-                      ? "Search USERS by name or email..."
-                      : "Search GROUPS by city or group name..."
-                  }
-                  value={q}
-                  /*
+        <div className="row d-flex justify-content-center align-items-center search-box flex-column">
+          <div className="search col-7">
+            <i className="fa fa-search"> </i>
+            <input
+              type="search"
+              name="search-form"
+              id="search-form"
+              className="form-control"
+              placeholder={
+                searchType === "user"
+                  ? "Search USERS by name or email..."
+                  : "Search GROUPS by city or group name..."
+              }
+              value={q}
+              /*
                         // set the value of our useState q
                         //  anytime the user types in the search box
                         */
-                  onChange={(e) => setQ(e.target.value)}
-                />
-              </div>
+              onChange={(e) => setQ(e.target.value)}
+            />
+          </div>
+          <div className="col-3 d-flex justify-content-around align-items-center">
+            <div className="btn-group w-100 mt-3">
+              <input
+                type="radio"
+                className="btn-check"
+                name="options"
+                id="searchUser"
+                autoComplete="off"
+                // checked="true"
+                defaultChecked={searchType === "user" ? true : false}
+                onChange={() => setSearchType("user")}
+              />
+              <label className="btn btn-primary" htmlFor="searchUser">
+                Users
+              </label>
+              <input
+                type="radio"
+                className="btn-check"
+                name="options"
+                id="searchGroup"
+                autoComplete="off"
+                defaultChecked={searchType === "group" ? true : false}
+                onChange={() => setSearchType("group")}
+              />
+              <label className="btn btn-primary" htmlFor="searchGroup">
+                Groups
+              </label>
             </div>
-            {/* busqueda acaba aquí */}
-            <div className="row pt-4  justify-content-center">
-              {/* resultados de la busqueda */}
-              <div className="col-10">
-                {searchType === "user" ? (
-                  <>
-                    {q.length >= 1 && searchUser(store.userList).length == 0 ? (
-                      <p className="text-center">
-                        {" "}
-                        No results for your query!{" "}
-                      </p>
-                    ) : (
-                      <> </>
-                    )}
-                    {q === "" ? (
-                      <p className="text-center">
-                        Use the bar above to search <br />
-                        Your results will be shown here!
-                      </p>
-                    ) : (
-                      <>
-                        {searchUser(store.userList).map((item, index) => (
-                          <SearchResult
-                            key={index}
-                            theName={item.name}
-                            id={index + 1}
-                            city={item.city}
-                            distance={item.distance}
-                            speed={item.speed}
-                            email={item.email}
-                          />
-                        ))}
-                      </>
-                    )}
-                  </>
+          </div>
+        </div>
+        {/* busqueda acaba aquí */}
+        <div className="row pt-2  justify-content-center">
+          {/* resultados de la busqueda */}
+          <div className="col-10">
+            {searchType === "user" ? (
+              <>
+                {q.length >= 1 && searchUser(store.userList).length == 0 ? (
+                  <p className="text-center"> No results for your query! </p>
+                ) : (
+                  <> </>
+                )}
+                {q === "" ? (
+                  <p className="text-center">
+                    Use the bar above to search <br />
+                    Your results will be shown here!
+                  </p>
                 ) : (
                   <>
-                    {q.length >= 1 &&
-                    searchGroup(store.groupDetails).length == 0 ? (
-                      <p className="text-center">
-                        {" "}
-                        No results for your query!{" "}
-                      </p>
-                    ) : (
-                      <> </>
-                    )}
-                    {q === "" ? (
-                      <p className="text-center">
-                        Use the bar above to search <br />
-                        Your results will be shown here!
-                      </p>
-                    ) : (
-                      <>
-                        {searchGroup(store.groupDetails).map((item, index) => (
-                          <SearchResult
-                            key={index}
-                            theName={item.name}
-                            id={index + 1}
-                            city={item.city}
-                            distance={item.distance}
-                            speed={item.speed}
-                          />
-                        ))}
-                      </>
-                    )}
+                    {searchUser(store.userList).map((item, index) => (
+                      <SearchResult
+                        key={index}
+                        theName={item.name}
+                        id={index + 1}
+                        city={item.city}
+                        distance={item.distance}
+                        speed={item.speed}
+                        email={item.email}
+                      />
+                    ))}
                   </>
                 )}
-              </div>
-            </div>
+              </>
+            ) : (
+              <>
+                {q.length >= 1 &&
+                searchGroup(store.groupDetails).length == 0 ? (
+                  <p className="text-center"> No results for your query! </p>
+                ) : (
+                  <> </>
+                )}
+                {q === "" ? (
+                  <p className="text-center">
+                    Use the bar above to search <br />
+                    Your results will be shown here!
+                  </p>
+                ) : (
+                  <>
+                    {searchGroup(store.groupDetails).map((item, index) => (
+                      <SearchResult
+                        key={index}
+                        theName={item.name}
+                        id={index + 1}
+                        city={item.city}
+                        distance={item.distance}
+                        speed={item.speed}
+                      />
+                    ))}
+                  </>
+                )}
+              </>
+            )}
           </div>
         </div>
       </div>
