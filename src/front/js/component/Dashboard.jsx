9aa2f5dd65ad0d0f532a13b1476577a623717577
iNelan <clpayala@gmail.com>
2022-07-20 08:51:48 +0000

    V.editprofileyFluxarreglado

--- src/front/js/component/Dashboard.jsx
+++ src/front/js/component/Dashboard.jsx
@@ -2,120 +2,90 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-export const Dashboard = () => {
+export const Dashboard = ({ name, id, distance, speed }) => {
+  const { store, actions } = useContext(Context);
   return (
-    <div className="">
-      <div className="row">
-        <div className="col-3 mt-5 ml-3">
-          <main className="dashboard_page_content">
-            <div className="dashboard_container_profilecard">
-              <div className="dashboard_page_card">
-                <div className="dashboard_page_text_content">
-                  <h2 className="title">{localStorage.getItem("name")}</h2>
-                  <p className="copy">{localStorage.getItem("city")}</p>
-                  <Link to={"/ViewProfileSettings/"}>
-                    <button className="dashboard_page_btn">
-                      Editar Perfil
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </main>
-        </div>
-        <div className="col-6">
-          <main className="dashboard_page_content">
-            <div className="dashboard_page_card">
-              <div className="dashboard_page_text_content">
-                <h2 className="title">Grupo Sevilla</h2>
-                <p className="copy">Miembros</p>
-                <Link to={"/ViewDetailDashboard/"}>
-                  <button className="dashboard_page_btn">Unirse</button>
-                </Link>
-              </div>
-            </div>
-            <div className="dashboard_page_card">
-              <div className="dashboard_page_text_content">
-                <h2 className="title">Grupo Málaga</h2>
-                <p className="copy">Miembros</p>
-                <Link to={"/ViewDetailDashboard/"}>
-                  <button className="dashboard_page_btn">Unirse</button>
-                </Link>
-              </div>
-            </div>
-            <div className="dashboard_page_card">
-              <div className="dashboard_page_text_content">
-                <h2 className="title">Grupo Madrid</h2>
-                <p className="copy">Miembros</p>
-                <Link to={"/ViewDetailDashboard/"}>
-                  <button className="dashboard_page_btn">Unirse</button>
-                </Link>
-              </div>
-            </div>
-            <div className="dashboard_page_card">
-              <div className="dashboard_page_text_content">
-                <h2 className="title">Grupo Barcelona</h2>
-                <p className="copy">Miembros</p>
-                <Link to={"/ViewDetailDashboard/"}>
-                  <button className="dashboard_page_btn">Unirse</button>
-                </Link>
-              </div>
-            </div>
-
-            <div className="dashboard_page_card">
-              <div className="dashboard_page_text_content">
-                <h2 className="title">Grupo Galicia</h2>
-                <p className="copy">Miembros</p>
-                <Link to={"/ViewDetailDashboard/"}>
-                  <button className="dashboard_page_btn">Unirse</button>
-                </Link>
-              </div>
-            </div>
-            <div className="dashboard_page_card">
-              <div className="dashboard_page_text_content">
-                <h2 className="title">Grupo Valencia</h2>
-                <p className="copy">Miembros</p>
-                <Link to={"/ViewDetailDashboard/"}>
-                  <button className="dashboard_page_btn">Unirse</button>
-                </Link>
-              </div>
-            </div>
-            <div className="dashboard_page_card">
-              <div className="dashboard_page_text_content">
-                <h2 className="title">Grupo Ceuta</h2>
-                <p className="copy">Miembros</p>
-                <Link to={"/ViewDetailDashboard/"}>
-                  <button className="dashboard_page_btn">Unirse</button>
-                </Link>
-              </div>
-            </div>
-            <div className="dashboard_page_card">
-              <div className="dashboard_page_text_content">
-                <h2 className="title">Grupo Melilla</h2>
-                <p className="copy">Miembros</p>
-                <Link to={"/ViewDetailDashboard/"}>
-                  <button className="dashboard_page_btn">Unirse</button>
-                </Link>
-              </div>
-            </div>
-          </main>
-        </div>
-        <div className="col-3 mt-5 ml-3">
-          <main className="dashboard_page_content">
-            <div className="dashboard_container_marketing">
-              <div className="dashboard_page_card">
-                <div className="dashboard_page_text_content">
-                  <h2 className="title"></h2>
-                  <p className="copy"></p>
-                  <Link to={"/ViewProfileSettings/"}>
-                    <button className="dashboard_page_btn">Comprar</button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </main>
-        </div>
+    // <div className="">
+    //   <div className="row">
+    //     <div className="col-6">
+    //       <main className="dashboard_page_content">
+    <div className="dashboard_page_card me-3">
+      <div className="dashboard_page_text_content">
+        <h2 className="title">Name:{name}</h2>
+        <p className="copy">Miembros</p>
+        <Link to={"/ViewDetailDashboard/"}>
+          <button className="dashboard_page_btn">Unirse</button>
+        </Link>
       </div>
     </div>
+    //         <div className="dashboard_page_card">
+    //           <div className="dashboard_page_text_content">
+    //             <h2 className="title">Grupo Málaga</h2>
+    //             <p className="copy">Miembros</p>
+    //             <Link to={"/ViewDetailDashboard/"}>
+    //               <button className="dashboard_page_btn">Unirse</button>
+    //             </Link>
+    //           </div>
+    //         </div>
+    //         <div className="dashboard_page_card">
+    //           <div className="dashboard_page_text_content">
+    //             <h2 className="title">Grupo Madrid</h2>
+    //             <p className="copy">Miembros</p>
+    //             <Link to={"/ViewDetailDashboard/"}>
+    //               <button className="dashboard_page_btn">Unirse</button>
+    //             </Link>
+    //           </div>
+    //         </div>
+    //         <div className="dashboard_page_card">
+    //           <div className="dashboard_page_text_content">
+    //             <h2 className="title">Grupo Barcelona</h2>
+    //             <p className="copy">Miembros</p>
+    //             <Link to={"/ViewDetailDashboard/"}>
+    //               <button className="dashboard_page_btn">Unirse</button>
+    //             </Link>
+    //           </div>
+    //         </div>
+
+    //         <div className="dashboard_page_card">
+    //           <div className="dashboard_page_text_content">
+    //             <h2 className="title">Grupo Galicia</h2>
+    //             <p className="copy">Miembros</p>
+    //             <Link to={"/ViewDetailDashboard/"}>
+    //               <button className="dashboard_page_btn">Unirse</button>
+    //             </Link>
+    //           </div>
+    //         </div>
+    //         <div className="dashboard_page_card">
+    //           <div className="dashboard_page_text_content">
+    //             <h2 className="title">Grupo Valencia</h2>
+    //             <p className="copy">Miembros</p>
+    //             <Link to={"/ViewDetailDashboard/"}>
+    //               <button className="dashboard_page_btn">Unirse</button>
+    //             </Link>
+    //           </div>
+    //         </div>
+    //         <div className="dashboard_page_card">
+    //           <div className="dashboard_page_text_content">
+    //             <h2 className="title">Grupo Ceuta</h2>
+    //             <p className="copy">Miembros</p>
+    //             <Link to={"/ViewDetailDashboard/"}>
+    //               <button className="dashboard_page_btn">Unirse</button>
+    //             </Link>
+    //           </div>
+    //         </div>
+    //         <div className="dashboard_page_card">
+    //           <div className="dashboard_page_text_content">
+    //             <h2 className="title">Grupo Melilla</h2>
+    //             <p className="copy">Miembros</p>
+    //             <Link to={"/ViewDetailDashboard/"}>
+    //               <button className="dashboard_page_btn">Unirse</button>
+    //             </Link>
+    //           </div>
+    //         </div>
+    //       </main>
+    //     </div>
+
+    //   </div>
+    // </div>
   );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,219 +1,257 @@
-const getState = ({ getStore, getActions, setStore }) => {
-  return {
-    store: {
-      message: null,
-      demo: [
-        {
-          title: "FIRST",
-          background: "white",
-          initial: "white",
-        },
-        {
-          title: "SECOND",
-          background: "white",
-          initial: "white",
-        },
-      ],
-      auth: false,
-      userDetails: {
-        name: null,
-        city: null,
-        speed: null,
-        distance: null,
-      },
-      groupDetails: [
-        {
-          name: "luis",
-        },
-      ],
-      userList: null,
-    },
-    actions: {
-      // Use getActions to call a function within a fuction
-      exampleFunction: () => {
-        getActions().changeColor(0, "green");
-      },
-
-      //Fetch para traer info grupos
-      groupInfo: () => {
-        fetch(process.env.BACKEND_URL + "/api/dashboard_info")
-          .then((response) => response.json())
-          .then((data) => {
-            setStore({
-              groupDetails: data,
-            });
-            // localStorage.setItem("groupName", data.name);
-            // .catch((err) => console.error(err));
-            console.log(getStore.groupDetails);
-          });
-      },
-      getUserList: () => {
-        fetch(process.env.BACKEND_URL + "/api/all_users")
-          .then((response) => response.json())
-          .then((data) => {
-            setStore({
-              userList: data,
-            });
-            // localStorage.setItem("groupName", data.name);
-            // .catch((err) => console.error(err));
-          });
-      },
-
-      registerUser: (values) => {
-        try {
-          const newUser = {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            message: null,
+            demo: [{
+                    title: "FIRST",
+                    background: "white",
+                    initial: "white",
+                },
+                {
+                    title: "SECOND",
+                    background: "white",
+                    initial: "white",
+                },
+            ],
+            auth: false,
+            userDetails: {
+                name: null,
+                city: null,
+                speed: null,
+                distance: null,
             },
-            body: JSON.stringify({
-              email: values.email,
-              password: values.password,
-              name: values.name,
-            }),
-          };
-          fetch(process.env.BACKEND_URL + "/api/register", newUser).then(
-            (response) => {
-              if (response.ok) {
+            groupDetails: [{
+                name: "luis",
+            }, ],
+            userList: null,
+            groupInfo: [{
+                    name: "null",
+                    city: "null",
+                    speed: "null",
+                    distance: "null",
+                },
                 {
-                  response.json().then((response) => {
-                    localStorage.setItem("token", response.access_token);
-                    localStorage.setItem("auth", true);
-                    // localStorage.setItem("name", data.name);
-                  });
-                  setStore({
-                    auth: true,
-                  });
-                  console.log(getStore().auth);
-                }
-              } else {
-                //lo que ocurre cuando la respuesta del endpoint no es OK
+                    name: "null",
+                    city: "null",
+                    speed: "null",
+                    distance: "null",
+                },
                 {
-                  alert("Parece que ese usuario ya existe"); //aquí habria que meter algo mas bonito que una alerta, pero de momento MVP
+                    name: "null",
+                    city: "null",
+                    speed: "null",
+                    distance: "null",
+                },
+                {
+                    name: "null",
+                    city: "null",
+                    speed: "null",
+                    distance: "null",
+                },
+                {
+                    name: "null",
+                    city: "null",
+                    speed: "null",
+                    distance: "null",
+                },
+                {
+                    name: "null",
+                    city: "null",
+                    speed: "null",
+                    distance: "null",
+                },
+            ],
+        },
+        actions: {
+            // Use getActions to call a function within a fuction
+            exampleFunction: () => {
+                getActions().changeColor(0, "green");
+            },
+
+            //Fetch para traer info grupos
+            groupInfo: () => {
+                fetch(process.env.BACKEND_URL + "/api/dashboard_info")
+                    .then((response) => response.json())
+                    .then((data) => {
+                        setStore({
+                            groupDetails: data,
+                        });
+                        // localStorage.setItem("groupName", data.name);
+                        // .catch((err) => console.error(err));
+                        console.log(getStore.groupDetails);
+                    });
+            },
+            getUserList: () => {
+                fetch(process.env.BACKEND_URL + "/api/all_users")
+                    .then((response) => response.json())
+                    .then((data) => {
+                        setStore({
+                            userList: data,
+                        });
+                        // localStorage.setItem("groupName", data.name);
+                        // .catch((err) => console.error(err));
+                    });
+            },
+
+            registerUser: (values) => {
+                try {
+                    const newUser = {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            email: values.email,
+                            password: values.password,
+                            name: values.name,
+                        }),
+                    };
+                    fetch(process.env.BACKEND_URL + "/api/register", newUser).then(
+                        (response) => {
+                            if (response.ok) {
+                                {
+                                    response.json().then((response) => {
+                                        localStorage.setItem("token", response.access_token);
+                                        localStorage.setItem("auth", true);
+                                        // localStorage.setItem("name", data.name);
+                                    });
+                                    setStore({
+                                        auth: true,
+                                    });
+                                    console.log(getStore().auth);
+                                }
+                            } else {
+                                //lo que ocurre cuando la respuesta del endpoint no es OK
+                                {
+                                    alert("Parece que ese usuario ya existe"); //aquí habria que meter algo mas bonito que una alerta, pero de momento MVP
+                                }
+                            }
+                        }
+                    );
+                } catch (error) {
+                    console.log("There is an error with the server", error);
                 }
-              }
-            }
-          );
-        } catch (error) {
-          console.log("There is an error with the server", error);
-        }
-      },
+            },
 
-      // loginUser: (values) => {
-      //   try {
-      //     const logUser = {
-      //       method: "POST",
-      //       headers: {
-      //         "Content-Type": "application/json",
-      //       },
-      //       body: JSON.stringify({
-      //         email: values.email,
-      //         password: values.password,
-      //       }),
-      //     };
-      //     fetch(process.env.BACKEND_URL + "/api/login", logUser).then(
-      //       (response) => {
-      //         if (response.ok) {
-      //           // {
-      //           response
-      //             .json()
-      //             .then((data) =>
-      //               localStorage.setItem("token", data.access_token)
-      //             );
-      //           setStore({
-      //             auth: true,
-      //             userDetails: {
-      //               name: data.name,
-      //               city: data.city,
-      //               speed: data.speed,
-      //               distance: data.distance,
-      //             },
-      //           });
-      //           console.log(getStore().auth);
-      //           console.log(getStore().userDetails);
-      //           // }
-      //         } else {
-      //           //lo que ocurre cuando la respuesta del endpoint no es OK
-      //           {
-      //             alert("Las credenciales no son correctas"); //aquí habria que meter algo mas bonito que una alerta, pero de momento MVP
-      //           }
-      //         }
-      //       }
-      //     );
-      //   } catch (error) {
-      //     console.log("There is an error with the server", error);
-      //   }
-      // },
-      loginUser: (values) => {
-        const logUser = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: values.email,
-            password: values.password,
-          }),
-        };
-        fetch(process.env.BACKEND_URL + "/api/login", logUser)
-          .then((response) => response.json())
-          .then((data) => {
-            // setStore({
-            //   userDetails: data,
-            //   auth: true,
-            // });
-            localStorage.setItem("token", data.access_token);
-            localStorage.setItem("name", data.name);
-            localStorage.setItem("city", data.city);
-            localStorage.setItem("speed", data.speed);
-            localStorage.setItem("auth", true);
-            setStore({
-              auth: true,
-            });
-          });
-      },
+            // loginUser: (values) => {
+            //   try {
+            //     const logUser = {
+            //       method: "POST",
+            //       headers: {
+            //         "Content-Type": "application/json",
+            //       },
+            //       body: JSON.stringify({
+            //         email: values.email,
+            //         password: values.password,
+            //       }),
+            //     };
+            //     fetch(process.env.BACKEND_URL + "/api/login", logUser).then(
+            //       (response) => {
+            //         if (response.ok) {
+            //           // {
+            //           response
+            //             .json()
+            //             .then((data) =>
+            //               localStorage.setItem("token", data.access_token)
+            //             );
+            //           setStore({
+            //             auth: true,
+            //             userDetails: {
+            //               name: data.name,
+            //               city: data.city,
+            //               speed: data.speed,
+            //               distance: data.distance,
+            //             },
+            //           });
+            //           console.log(getStore().auth);
+            //           console.log(getStore().userDetails);
+            //           // }
+            //         } else {
+            //           //lo que ocurre cuando la respuesta del endpoint no es OK
+            //           {
+            //             alert("Las credenciales no son correctas"); //aquí habria que meter algo mas bonito que una alerta, pero de momento MVP
+            //           }
+            //         }
+            //       }
+            //     );
+            //   } catch (error) {
+            //     console.log("There is an error with the server", error);
+            //   }
+            // },
+            loginUser: (values) => {
+                const logUser = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        email: values.email,
+                        password: values.password,
+                    }),
+                };
+                fetch(process.env.BACKEND_URL + "/api/login", logUser)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        // setStore({
+                        //   userDetails: data,
+                        //   auth: true,
+                        // });
+                        localStorage.setItem("token", data.access_token);
+                        localStorage.setItem("name", data.name);
+                        localStorage.setItem("city", data.city);
+                        localStorage.setItem("speed", data.speed);
+                        localStorage.setItem("auth", true);
+                        setStore({
+                            auth: true,
+                        });
+                    });
+            },
 
-      logout: () => {
-        localStorage.removeItem("token");
-        localStorage.removeItem("auth");
-        localStorage.removeItem("name");
-        setStore({
-          auth: false,
-        });
-      },
+            logout: () => {
+                localStorage.removeItem("token");
+                localStorage.removeItem("auth");
+                localStorage.removeItem("name");
+                setStore({
+                    auth: false,
+                });
+            },
 
-      getMessage: async () => {
-        try {
-          // fetching data from the backend
-          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
-          const data = await resp.json();
-          setStore({
-            message: data.message,
-          });
-          // don't forget to return something, that is how the async resolves
-          return data;
-        } catch (error) {
-          console.log("Error loading message from backend", error);
-        }
-      },
-      changeColor: (index, color) => {
-        //get the store
-        const store = getStore();
+            getMessage: async () => {
+                try {
+                    // fetching data from the backend
+                    const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+                    const data = await resp.json();
+                    setStore({
+                        message: data.message,
+                    });
+                    // don't forget to return something, that is how the async resolves
+                    return data;
+                } catch (error) {
+                    console.log("Error loading message from backend", error);
+                }
+            },
+            changeColor: (index, color) => {
+                //get the store
+                const store = getStore();
 
-        //we have to loop the entire demo array to look for the respective index
-        //and change its color
-        const demo = store.demo.map((elm, i) => {
-          if (i === index) elm.background = color;
-          return elm;
-        });
+                //we have to loop the entire demo array to look for the respective index
+                //and change its color
+                const demo = store.demo.map((elm, i) => {
+                    if (i === index) elm.background = color;
+                    return elm;
+                });
 
-        //reset the global store
-        setStore({
-          demo: demo,
-        });
-      },
-    },
-  };
+                //reset the global store
+                setStore({
+                    demo: demo,
+                });
+            },
+        },
+    };
 };
 
 export default getState;
