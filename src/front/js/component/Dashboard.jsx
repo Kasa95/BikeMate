9c55bfe53cf9f369d23c71ae0b9a97453100c891
mpadillazeravla <mikepadilla82@gmail.com>
2022-07-28 16:28:13 +0000

    cambios en backend y dashboard

--- src/front/js/component/Dashboard.jsx
+++ src/front/js/component/Dashboard.jsx
@@ -2,11 +2,10 @@ import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-export const Dashboard = ({ name, id, distance, speed }) => {
+export const Dashboard = ({ name, id, distance, speed, city, routetype }) => {
   const { store, actions } = useContext(Context);
   useEffect(() => {
     actions.groupInfo();
-    actions.userInfo();
   }, []);
   return (
     // <div className="">
@@ -15,8 +14,11 @@ export const Dashboard = ({ name, id, distance, speed }) => {
     //       <main className="dashboard_page_content">
     <div className="dashboard_page_card me-3">
       <div className="dashboard_page_text_content">
-        <h2 className="title">Name:{name}</h2>
-        <p className="copy">Miembros</p>
+        <h2 className="title">{name}</h2>
+        <p className="copy">City: {city}</p>
+        <p className="copy">Average distance: {distance} km</p>
+        <p className="copy">Average speed: {speed} km/h</p>
+        <p className="copy">Route type: {routetype} </p>
         <Link to={"/ViewDetailDashboard/"}>
           <button className="dashboard_page_btn">Unirse</button>
         </Link>
