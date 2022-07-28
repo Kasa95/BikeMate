3176327d8df26555114504bacd5484f593c1f5d1
mpadillazeravla <mikepadilla82@gmail.com>
2022-07-28 22:56:42 +0000

    cambios en viewdashboard

--- src/front/js/component/Profile.jsx
+++ src/front/js/component/Profile.jsx
@@ -9,20 +9,46 @@ export const Profile = (props) => {
   }, []);
 
   return (
-    <div className="dashboard_page_card">
-      <div className="dashboard_page_text_content">
-        <h2 className="title">
-          About me:<br></br>
-          {store.profile.name}
-        </h2>
-        <p className="copy">City: {store.profile.city}</p>
-        <p className="copy">Average distance: {store.profile.distance} km</p>
-        <p className="copy">Average speed: {store.profile.speed} km/h</p>
-        <p className="copy">Route type: {store.profile.routetype} </p>
-        <Link to={"/ViewProfileSettings/"}>
-          <button className="dashboard_page_btn">Editar Perfil</button>
-        </Link>
-      </div>
-    </div>
+    <>
+      {" "}
+      {store.profile.city != 0 ? (
+        <>
+          <div className="dashboard_page_card">
+            <div className="dashboard_page_text_content">
+              <h2 className="title">
+                About me:<br></br>
+                <br></br>
+                {store.profile.name}
+              </h2>
+              <p className="copy">UPDATE YOUR PROFILE FOR YOUR BIKEMATES</p>
+              <Link to={"/ViewProfileSettings/"}>
+                <button className="dashboard_page_btn">
+                  Edit your profile
+                </button>
+              </Link>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="dashboard_page_card">
+          <div className="dashboard_page_text_content">
+            <h2 className="title">
+              About me:<br></br>
+              <br></br>
+              {store.profile.name}
+            </h2>
+            <p className="copy">City: {store.profile.city}</p>
+            <p className="copy">
+              Average distance: {store.profile.distance} km
+            </p>
+            <p className="copy">Average speed: {store.profile.speed} km/h</p>
+            <p className="copy">Route type: {store.profile.routetype} </p>
+            <Link to={"/ViewProfileSettings/"}>
+              <button className="dashboard_page_btn">Edit your profile</button>
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
