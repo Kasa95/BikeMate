366527dd6a59b052cbc6e3bdc09188d52e32766c
Kasa95 <nachocasanova95@gmail.com>
2022-07-29 14:49:33 +0000

    mejoras register y login

--- migrations/versions/bce3aa4847b7_.py
+++ migrations/versions/bce3aa4847b7_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 403dc5b8706d
+Revision ID: bce3aa4847b7
 Revises: 
-Create Date: 2022-07-28 19:39:26.501211
+Create Date: 2022-07-29 13:11:27.889931
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '403dc5b8706d'
+revision = 'bce3aa4847b7'
 down_revision = None
 branch_labels = None
 depends_on = None
---
+++ src/front/js/pages/register2.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { Formik, input, Form, useFormik } from "formik";
+import * as Yup from "yup";
+import { Context } from "../store/appContext";
+import { newUserSchema } from "../validations/schemas";
+
+export const Register2 = () => {
+  const { store, actions } = useContext(Context);
+  const { values, handleBlur, errors, handleChange, handleSubmit, touched } =
+    useFormik({
+      initialValues: {
+        name: "",
+        email: "",
+        password: "",
+      },
+      validationSchema: newUserSchema,
+      onSubmit: async (values) => {
+        const response = await actions.registerUser(values);
+      },
+    });
+  console.log(errors);
+  console.log(touched);
+  console.log(values);
+  return (
+    <div className="container py-5 h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div className="card shadow-2-strong rounded-3">
+            <div className="card-body p-5 text-center">
+              <h1 className="mb-5"> Sign Up </h1>
+              {store.auth === true ? (
+                <Navigate to="/ViewDashboard" />
+              ) : (
+                <form onSubmit={handleSubmit}>
+                  <div className="form-outline mb-4">
+                    {errors.name && touched.name ? (
+                      <label htmlFor="name" className="text-danger form-label">
+                        {errors.name}
+                      </label>
+                    ) : (
+                      <label htmlFor="name" className="form-label">
+                        Name
+                      </label>
+                    )}
+
+                    <input
+                      className="form-control form-control-lg"
+                      id="name"
+                      name="name"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-outline mb-4">
+                    {errors.email && touched.email ? (
+                      <label htmlFor="email" className="text-danger form-label">
+                        {errors.email}
+                      </label>
+                    ) : (
+                      <label htmlFor="email" className="form-label">
+                        Email
+                      </label>
+                    )}
+                    <input
+                      className="form-control form-control-lg"
+                      id="email"
+                      name="email"
+                      type="email"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-outline mb-4">
+                    {errors.password && touched.password ? (
+                      <label
+                        htmlFor="password"
+                        className="text-danger form-label"
+                      >
+                        {errors.password}
+                      </label>
+                    ) : (
+                      <label htmlFor="password" className="form-label">
+                        Password
+                      </label>
+                    )}
+                    <input
+                      className="form-control form-control-lg"
+                      id="password"
+                      name="password"
+                      type="password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="d-flex justify-content-center mb-4">
+                    <label className="form-check-label">
+                      By registering, you agree to the{" "}
+                      <Link to="#"> Terms & Conditions </Link>
+                    </label>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-lg btn-block"
+                  >
+                    REGISTER
+                  </button>
+                </form>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
