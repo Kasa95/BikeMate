d5d1f52fdce42328979d333689be9cbc3920798e
mpadillazeravla <mikepadilla82@gmail.com>
2022-08-08 17:52:24 +0000

    fixed bug with mail user and pass

--- migrations/versions/189de0220b83_.py
+++ migrations/versions/189de0220b83_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: d6b600a95c53
+Revision ID: 189de0220b83
 Revises: 
-Create Date: 2022-08-08 08:25:25.306564
+Create Date: 2022-08-08 17:43:20.091361
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'd6b600a95c53'
+revision = '189de0220b83'
 down_revision = None
 branch_labels = None
 depends_on = None
